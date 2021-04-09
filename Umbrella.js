0c291dfbcacc7efa2478b8140d0c9aa0e1752c27
Pranvi <75794298+Pranvi10@users.noreply.github.com>
2021-04-09 11:21:34 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,18 @@
+class Umbrella {
+    constructor(x,y){
+        var options = {
+            isStatic: true,
+        }
+        this.image = loadImage("walking_1.png");
+
+        this.umbrella = Bodies.circle(x,y,70,options);
+        this.radius = 70;
+        World.add(world, this.umbrella)
+    }
+
+    display(){
+        var pos = this.umbrella.position;
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y+70,300,300);
+    }
+}
